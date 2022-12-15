51144617ad7f2732fb57b9b6c4ffe27ec14cef1c
tejaskatkade <tejaskatkade44@gmail.com>
2022-12-15 22:20:13 +0530

    15-12-2022

---
+++ Stack/usingArray/program1.c
@@ -0,0 +1,25 @@
+//15-12-22 (1)
+
+#include<stdio.h>
+
+int top;
+int arr[5];
+
+void push(int data){
+    top++;
+    arr[top] = data;
+}
+void main(){
+
+    printf("Enter data to push in the stack\n");
+    int data;
+    for(int i=0 i<5; i++){
+        scanf("%d",&data);
+        push(data);
+    }
+
+    for(int i = top; i >0; i--){
+        printf("%d\n",arr[i]);
+    }
+    
+}
